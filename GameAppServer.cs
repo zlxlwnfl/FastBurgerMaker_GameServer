347dbf02f433f3254b5bbb82c1f6a569ccb31491
zlxlwnfl <zlxlwnfl@naver.com>
2022-10-01 02:48:14 +0900

    request 관련 기본 셋팅/USER_READY 요청키 관련 구현

---
+++ GameAppServer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SuperSocket.SocketBase;
+using SuperSocket.SocketBase.Command;
+using SuperSocket.SocketBase.Protocol;
+
+namespace FastBurgerMaker_GameServer
+{
+    public class GameAppSession: AppSession<GameAppSession, GameRequestInfo>
+    {
+
+    }
+
+    public class GameAppServer: AppServer<GameAppSession, GameRequestInfo>
+    {
+        public GameAppServer(): 
+            base(new DefaultReceiveFilterFactory<GameReceiveFilter, GameRequestInfo>())
+        {
+
+        }
+    }
+}
